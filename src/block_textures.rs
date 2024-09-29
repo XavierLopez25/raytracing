ea8eb3e4a2888abddc48413114f604d4ebc1a51a
XavierLopez25 <thexlopez59@gmail.com>
2024-09-29 14:33:20 -0600

    feat: added module for block textures

--- src/block_textures.rs
+++ src/block_textures.rs
@@ -4,30 +4,70 @@ use std::sync::Arc;
 
 pub struct Textures {
     pub grass_material: Material,
-    // pub water_material: Material,
-    // pub stone_material: Material,
-    // Add other materials here
+    pub obsidian_material: Material,
+    pub bookshelf_material: Material,
+    pub cactus_material: Material,
+    pub dirt_material: Material,
+    pub oak_log_material: Material,
+    pub oak_planks_material: Material,
+    pub sand_material: Material,
+    pub shroomlight_material: Material,
+    pub smoker_material: Material,
 }
 
 impl Textures {
     pub fn new() -> Self {
-        let grass_texture = Arc::new(Texture::new("assets/grass_signed.png"));
+        let grass_texture = Arc::new(Texture::new("assets/grass.png"));
         let grass_material =
             Material::new_with_texture(1.0, [1.0, 0.05, 0.0, 0.0], 0.0, grass_texture);
 
-        // let water_texture = Arc::new(Texture::new("assets/water_signed.png"));
-        // let water_material =
-        //     Material::new_with_texture(0.5, [0.5, 0.5, 0.5, 0.5], 1.33, water_texture);
+        let obsidian_texture = Arc::new(Texture::new("assets/obsidian.png"));
+        let obsidian_material =
+            Material::new_with_texture(0.01, [0.45, 0.01, 0.01, 0.0], 1.5, obsidian_texture);
 
-        // let stone_texture = Arc::new(Texture::new("assets/stone_signed.png"));
-        // let stone_material =
-        //     Material::new_with_texture(0.8, [0.1, 0.9, 0.0, 0.0], 0.0, stone_texture);
+        let bookshelf_texture = Arc::new(Texture::new("assets/bookshelf.png"));
+        let bookshelf_material =
+            Material::new_with_texture(0.8, [0.60, 0.40, 0.05, 0.0], 0.0, bookshelf_texture);
+
+        let cactus_texture = Arc::new(Texture::new("assets/cactus.png"));
+        let cactus_material =
+            Material::new_with_texture(0.85, [0.75, 0.85, 0.15, 0.0], 0.0, cactus_texture);
+
+        let dirt_texture = Arc::new(Texture::new("assets/dirt.png"));
+        let dirt_material =
+            Material::new_with_texture(1.0, [1.0, 0.05, 0.0, 0.0], 0.0, dirt_texture);
+
+        let oak_log_texture = Arc::new(Texture::new("assets/oak_log.png"));
+        let oak_log_material =
+            Material::new_with_texture(0.9, [0.55, 0.40, 0.05, 0.0], 0.0, oak_log_texture);
+
+        let oak_planks_texture = Arc::new(Texture::new("assets/oak_planks.png"));
+        let oak_planks_material =
+            Material::new_with_texture(1.0, [0.95, 0.1, 0.0, 0.0], 1.3, oak_planks_texture);
+
+        let sand_texture = Arc::new(Texture::new("assets/sand.png"));
+        let sand_material =
+            Material::new_with_texture(1.0, [0.95, 0.90, 0.10, 0.0], 0.0, sand_texture);
+
+        let shroomlight_texture = Arc::new(Texture::new("assets/shroomlight.png"));
+        let shroomlight_material =
+            Material::new_with_texture(0.5, [1.0, 0.9, 0.1, 0.0], 0.0, shroomlight_texture);
+
+        let smoker_texture = Arc::new(Texture::new("assets/smoker.png"));
+        let smoker_material =
+            Material::new_with_texture(0.7, [0.35, 0.25, 0.05, 0.0], 0.0, smoker_texture);
 
         Self {
             grass_material,
-            // water_material,
-            // stone_material,
-            // Initialize other materials here
+            obsidian_material,
+            bookshelf_material,
+            cactus_material,
+            dirt_material,
+            oak_log_material,
+            oak_planks_material,
+            sand_material,
+            shroomlight_material,
+            smoker_material,
         }
     }
 }
